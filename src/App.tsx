e37249dfc9f39d136f97f56844e7cb290bbaf8d3
Romeoteni188 <romeoteni093@gmail.com>
2024-09-30 11:35:11 -0600

    Resolviendo problemas con las rutas y algunos archivos

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,11 @@ import Menu from './components/Menu';
 import Page from './pages/Page';
 import Registrar from './components/RomeoComp/Registrar/Registrar';
 import ListadoGeneral from './components/RomeoComp/Comunidad/ListadoGeneral';
-
 import Visualizar from './components/RomeoComp/Proyecto/Visualizar';
 import Panel from './components/RomeoComp/Panel/Panel';
-
+import Comunidad from './components/RomeoComp/Comunidad/Comunidad1';
+import VisualizarComunidades from './pages/MarcoPaginas/AgregarComunidad';
+import { Configuraciones } from './pages/MarcoPaginas/Configuraciones';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -26,20 +27,11 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
-
-/* import '@ionic/react/css/palettes/dark.always.css'; */
-/* import '@ionic/react/css/palettes/dark.class.css'; */
-
+/* Dark Mode Styles (Optional) */
+import '@ionic/react/css/palettes/dark.class.css';
 
 /* Theme variables */
 import './theme/variables.css';
-
 
 setupIonicReact();
 
@@ -66,15 +58,17 @@ const App: React.FC = () => {
               <Panel />
             </Route>
             <Route path="/comunidad" exact={true}>
-              <Comunidad1 />
+              <Comunidad />
             </Route>
-            
-              
-            
+            <Route path="/AgregarComunidades" exact={true}>
+              <VisualizarComunidades />
+            </Route>
+            <Route path="/Configuraciones" exact={true}>
+              <Configuraciones />
+            </Route>
             <Route path="/Registrar" exact={true}>
-              <Registrar/>
+              <Registrar />
             </Route>
-         
           </IonRouterOutlet>
         </IonSplitPane>
       </IonReactRouter>
